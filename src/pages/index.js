9a77588b8b45d48307ad31ef39e78e4d50364d11
elepampam <budisuryadarma0407@gmail.com>
2018-01-10 15:50:18 +0800

    changing hexo to gatsbyJS

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import Link from 'gatsby-link'
+
+class IndexPage extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    )
+  }
+}
+
+export default IndexPage

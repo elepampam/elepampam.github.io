2e66440177c0d22290f298fa0955bee3a4790f52
elepampam <budisuryadarma0407@gmail.com>
2018-01-10 17:52:16 +0800

    set first-post

---
+++ src/pages/2018-01-10-first-post/index.md
@@ -0,0 +1,6 @@
+---
+path: '/first-post'
+title: 'Hello World'
+---
+
+Hello World
---
+++ src/templates/post.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react'
+import Helmet from 'react-helmet'
+
+export default function Template({data}){
+  const post = data.markdownRemark
+  return(
+    <div>
+      <h1>
+        {post.frontmatter.title}
+      </h1>
+      <hr />
+      <div dangerouslySetInnerHTML={{__html: post.html}}/>              
+    </div>
+  )
+}
+
+export const postQuery = graphql`
+  query BlogPostByPath($path: String!){
+    markdownRemark(frontmatter: { path: {eq: $path} }){
+      html
+      frontmatter{
+        path
+        title
+      }
+    }
+  }
+`
